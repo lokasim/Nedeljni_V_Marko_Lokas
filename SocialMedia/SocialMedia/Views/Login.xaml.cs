600f2dceed6f5e08d9e9a20fe9fa6ab39815903b
markolokas <lokasmarko.im@gmail.com>
2020-09-05 17:56:59 +0200

    Created a form for user login and registration

---
+++ SocialMedia/SocialMedia/Views/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SocialMedia.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            this.Language = XmlLanguage.GetLanguage("sr-SR");
+        }
+
+
+
+
+        private void DragMe(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
